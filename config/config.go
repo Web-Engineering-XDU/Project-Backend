594c19d8ecafb7687d7efa192401afce1192b35b
Avery-zzZ <3081169057@qq.com>
2023-05-28 16:17:12 +0800

    fix

--- config/config.go
+++ config/config.go
@@ -3,16 +3,11 @@ package config
 type (
 	Config struct {
 		MySQL   `yaml:"mysql"`
-		RssPath `yaml:"rss"`
 	}
 	MySQL struct {
 		Host     string `yaml:"host" env-default:"localhost"`
 		Port     string `yaml:"port" env-default:"3306"`
 		User     string `yaml:"user" env-required:"true"`
 		Password string `yaml:"password" env-required:"true"`
-	}
-
-	RssPath struct {
-		Path string `yaml:"path" env-required:"true"`
 	}
 )

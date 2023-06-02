6adfa3b23a510eadcf90a6e11fd713c034806540
Avery-zzZ <3081169057@qq.com>
2023-06-02 16:00:24 +0800

    Revert "https"
    This reverts commit d07d01f68c5332274706b1a8e3a7bf84b00e5a37.

--- cmd/main.go
+++ cmd/main.go
@@ -28,7 +28,7 @@ type Config struct {
 // @license.name  Apache 2.0
 // @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host      huggo.guchen.work:8080
+// @host      spm.guchen.work:8080
 // @BasePath  /
 
 // @securityDefinitions.basic  BasicAuth

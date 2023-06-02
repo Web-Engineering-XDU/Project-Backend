bf82309050f4a06ca859e704cf4db4a38d5e6e5a
Avery-zzZ <3081169057@qq.com>
2023-06-02 15:44:02 +0800

    https

--- app/service/huggo.go
+++ app/service/huggo.go
@@ -1,6 +1,9 @@
 package service
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/Web-Engineering-XDU/Project-Backend/app/models"
 	agentsystem "github.com/Web-Engineering-XDU/Project-Backend/app/service/agent_system"
 	"github.com/Web-Engineering-XDU/Project-Backend/config"
@@ -17,17 +20,22 @@ func New(huggoConfig config.Config, setController func(*gin.Engine, *agentsystem
 	agents := agentsystem.NewAgentCollection()
 	eventHdl := agentsystem.NewEventHandler()
 	ret := &Huggo{
-		config: huggoConfig,
-		agentSys:  agentsystem.NewAgentSystem(&agents, &eventHdl),
+		config:   huggoConfig,
+		agentSys: agentsystem.NewAgentSystem(&agents, &eventHdl),
 	}
 	ret.ginServer = setController(gin.Default(), ret.agentSys.Agents)
 	return ret
 }
 
 func (huggo *Huggo) Run(port string) {
-	if err:=models.InitDB(huggo.config.MySQL) ; err!=nil{
+	if err := models.InitDB(huggo.config.MySQL); err != nil {
 		panic(err)
 	}
 	go huggo.agentSys.Run()
-	huggo.ginServer.Run(port)
+
+	ex, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	huggo.ginServer.RunTLS(":8848", filepath.Dir(ex)+"/spm.guchen.work_bundle.crt", filepath.Dir(ex)+"/spm.guchen.work.key")
 }
--- go.mod
+++ go.mod
@@ -70,6 +70,7 @@ require (
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.9 // indirect
+	github.com/unrolled/secure v1.13.0
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
 	golang.org/x/crypto v0.7.0 // indirect
 	golang.org/x/net v0.8.0 // indirect

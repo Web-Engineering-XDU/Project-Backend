1d608d1f98710509e3a2650fe1125b600fe6af73
Avery-zzZ <3081169057@qq.com>
2023-06-05 15:13:10 +0800

    fix

--- app/service/agent_system/api.go
+++ app/service/agent_system/api.go
@@ -143,11 +143,72 @@ func (ac *AgentCollection) SetAgentRelation(agentId int, srcs, dsts []int) error
 		return err
 	}
 
+	deleted, added := findMissingAndExtraNumbers(agent.SrcAgentId, srcs)
+
 	agent.Mutex.Lock()
 	agent.SrcAgentId = srcs
 	agent.DstAgentId = dsts
 	agent.Mutex.Unlock()
+
+	for _, v := range deleted {
+		ac.agentMap[v].Mutex.Lock()
+		ac.agentMap[v].DstAgentId = removeFromSlice(ac.agentMap[v].DstAgentId, v)
+		ac.agentMap[v].Mutex.Unlock()
+	}
+	for _, v := range added {
+		ac.agentMap[v].Mutex.Lock()
+		ac.agentMap[v].DstAgentId = append(agent.DstAgentId, v)
+		ac.agentMap[v].Mutex.Unlock()
+	}
 	return nil
+}
+
+func removeFromSlice(slice []int, num int) []int {
+	index := -1
+
+	// 找到要删除的元素的索引
+	for i, val := range slice {
+		if val == num {
+			index = i
+			break
+		}
+	}
+
+	// 如果找到了要删除的元素，则通过切片操作删除它
+	if index != -1 {
+		slice = append(slice[:index], slice[index+1:]...)
+	}
+
+	return slice
+}
+
+func findMissingAndExtraNumbers(a, b []int) ([]int, []int) {
+	missing := []int{}  // 存储 b 中缺失的数
+	extra := []int{}    // 存储 b 中多余的数
+	counts := make(map[int]int) // 记录 a 中每个数出现的次数
+
+	// 统计 a 中每个数出现的次数
+	for _, num := range a {
+		counts[num]++
+	}
+
+	// 检查 b 中的每个数在 a 中是否存在
+	for _, num := range b {
+		if count, found := counts[num]; found && count > 0 {
+			counts[num]--  // 在 counts 中减去对应的数的计数
+		} else {
+			extra = append(extra, num)  // 如果在 counts 中找不到或计数已经为 0，则将其添加到 extra 中
+		}
+	}
+
+	// 将在 counts 中仍有剩余计数的数添加到 missing 中
+	for num, count := range counts {
+		if count > 0 {
+			missing = append(missing, num)
+		}
+	}
+
+	return missing, extra
 }
 
 func DryRunAgent(a *models.Agent, msg Message) ([]*Message, error) {
